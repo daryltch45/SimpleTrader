b75e95af820c7a37019fde2a04bda001b1636c0c
Daryl Tchienang <SESA636891@se.com>
2023-01-11 11:08:29 +0100

    GRUD operations

---
+++ SimpleTrader.EntityFramework/Services/GenericDataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.EntityFramework.Services
+{
+    internal class GenericDataService
+    {
+    }
+}
---
+++ SimpleTrader/Model/DomainObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleTrader.Model
+{
+    internal class DomainObject
+    {
+    }
+}
