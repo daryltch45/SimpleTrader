64d494dc3f0c54715cf8ac4def0202eaa72ab853
daryltch45 <daryl.tchienang@gmail.com>
2023-01-29 18:00:53 +0100

    MajorIndex got implemented: AAPL, FONR, MNKD, MajorIndexServices

---
+++ SimpleTrader.WPF/ViewModels/MajorIndexViewModel.cs
@@ -0,0 +1,55 @@
+﻿using SimpleTrader.Domain.Model;
+using SimpleTrader.Domain.Service;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.ViewModels
+{
+  public class MajorIndexViewModel
+  {
+    private readonly IMajorIndexService _majorIndexService; 
+    public MajorIndex AAPL { get; set; }
+    public MajorIndex FONR { get; set; }
+    public MajorIndex MNKD { get; set; }
+
+
+    public MajorIndexViewModel(IMajorIndexService majorIndexService)
+    {
+      _majorIndexService = majorIndexService;
+    }
+    //Reason why we initialised a new function for the lading of datas: 
+    //If we initialize it in the constructor, we are gonna wait the Informations to be loaded, every single time, we will inintilize MajrindexViewModel.
+    //but in this way, we can get our MajorindexViewModel then load the informations
+    public static MajorIndexViewModel LoadMajorIndexViewModel(IMajorIndexService majorIndexService)
+    {
+      MajorIndexViewModel majorIndexViewModel = new MajorIndexViewModel(majorIndexService);
+      majorIndexViewModel.LoadMajorIndexes();
+      return majorIndexViewModel; 
+    }
+    private void LoadMajorIndexes()
+    {
+      _majorIndexService.GetMajorIndex(MajorIndexType.AAPL).ContinueWith((task) =>
+      {
+        if(task.Exception == null)
+        {
+          AAPL = task.Result; 
+        }
+      });
+
+      _majorIndexService.GetMajorIndex(MajorIndexType.FONR).ContinueWith((task) =>
+      {
+        if (task.Exception == null)
+        {
+          FONR = task.Result;
+        }
+      });
+
+      _majorIndexService.GetMajorIndex(MajorIndexType.MNKD).ContinueWith((task) =>
+      {
+        if (task.Exception == null)
+        {
+          MNKD = task.Result;
+        }
+      });
+    }
+  }
+}
